3e99d11695b441b9c06f9e1e30fa77c488b822c2
Hanny Carballo <launchx00194@innovaccion.mc>
2022-07-03 22:39:31 -0500

    Taller de introducción a Next.js

---
+++ Cursos/nextjs initial workshop/pages/index.tsx
@@ -0,0 +1,9 @@
+import type {NextPage} from "next";
+
+import styles from "../styles/Home.module.css";
+
+ const HomePage: NextPage = () => {
+  return <p className={styles.text}>Hello future</p>;
+};
+
+export default HomePage;
